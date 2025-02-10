c4d7a712e3100a65ba5613f4cdf580abde6ae5be
dasith <dasith@ebadfd.tech>
2025-02-10 07:23:09 +0530

    chore: updated the secrets

--- clusters/misaki/synapse/.hack/config.sh
+++ clusters/misaki/synapse/.hack/config.sh
@@ -3,4 +3,4 @@ kubectl create secret generic synapse -n matrix \
     --from-file=synaps-conf/log.config \
     --dry-run=client -o yaml > configurations.yaml
 
-sops --encrypt --in-place secrets.yaml
+sops --encrypt --in-place configurations.yaml
