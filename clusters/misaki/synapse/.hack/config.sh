dfd4e8106897386013d6b3253c4098e514f7c023
dasith <dasith@ebadfd.tech>
2025-07-20 10:13:48 +0530

    chore: remove synapse

--- clusters/misaki/synapse/.hack/config.sh
+++
@@ -1,6 +0,0 @@
-kubectl create secret generic synapse -n matrix \
-    --from-file=synaps-conf/home-server.yaml \
-    --from-file=synaps-conf/log.config \
-    --dry-run=client -o yaml > configurations.yaml
-
-sops --encrypt --in-place configurations.yaml
